1e2e468f261769b643e11bd82a38d3607e1cdff9
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2022-12-11 13:10:56 +0900

    ver1.3

--- app/src/main/java/com/kitadai31/MonakeyClearActivity.java
+++ app/src/main/java/com/kitadai31/MonakeyClearActivity.java
@@ -1,14 +1,13 @@
 package com.kitadai31;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+/*Ver 1.3*/
 public class MonakeyClearActivity extends Activity {
 
     @Override
@@ -21,31 +20,32 @@ public class MonakeyClearActivity extends Activity {
 
         //文字表示
         TextView text = new TextView(this);
-        text.setText("MonaKey Remover 1.0\n削除ボタンをタップするとMonaKey(鍵)が削除されます。\n\n【警告】\n鍵の取得を同一IPアドレスで短期間に複数回繰り返すと、「不正取得を検知しました」規制でしばらく鍵が取得できなくなります。\n鍵を削除した後は、問題となった携帯回線での書き込み、問題となった板への書き込みなどに注意しましょう。\nまた、荒らし行為やそれに類する行為には絶対に使用しないでください。\n\n以上を理解した上で削除ボタンをタップしてください。");
+        text.setText("MonakeyClearActivity 1.3\n削除ボタンをタップするとMonaKey(鍵)が削除されます。\n\n【警告】\n鍵を削除しても、板単位で行われている規制を回避できるわけではありません。あなたの環境(回線・専ブラ・機種の組合わせ)で現在規制されている板へ書き込めば、何度でもBANされます。\n鍵を削除した後は、問題となった板への書き込みに注意しましょう。\n特に、鍵の取得を同一IPアドレスで短期間に複数回繰り返すと、「不正取得を検知しました」規制でしばらく鍵が取得できなくなるので注意が必要です。\n\nまた、荒らしなどの迷惑行為には絶対に使用しないでください。\n\n以上を理解した上で削除ボタンをタップしてください。");
         linearLayout.addView(text);
 
-        //削除ボタン
+        //削除ボタン表示
         Button button = new Button(this);
         button.setText("削除");
-        TextView text2 = new TextView(this); //ボタンを押したら表示されるテキストを生成しておく
-        text2.setText("削除しました。");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //「削除しました」メッセージを出すようにすると削除ボタン二度押しでクラッシュするのでボタン無効化
-                button.setEnabled(false);
-                //MonaKey削除
-                SharedPreferences pref = getSharedPreferences("2chapi", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = pref.edit();
-                editor.remove("2chapi_monakey");
-                editor.commit();
-                //削除メッセージ表示
-                linearLayout.addView(text2);
-            }
-        });
         button.setLayoutParams(new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT));
         linearLayout.addView(button);
+
+        //ボタンを押したら表示される用のtext2を生成しておく
+        TextView text2 = new TextView(this);
+        text2.setText("削除しました。");
+
+        //削除ボタンの動作
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ボタン無効化
+                button.setEnabled(false);
+                //MonaKey削除
+                getSharedPreferences("2chapi", 0).edit().remove("2chapi_monakey").commit();
+                //削除メッセージ表示
+                linearLayout.addView(text2);
+            }
+        });
     }
 }
